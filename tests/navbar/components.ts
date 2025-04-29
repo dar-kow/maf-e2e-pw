b3211bdc1ad8f5344198d0816b5a437a74830314
Darek <darecki9k@gmail.com>
2025-04-29 19:40:15 +0200

    Add Components definition for navbar elements in Playwright tests

---
+++ tests/navbar/components.ts
@@ -0,0 +1,8 @@
+export const Components = {
+  root: "nav.navbar",
+  sectionTitle: ".section-title",
+  navbarActions: ".navbar-actions",
+  navbarIcon: ".navbar-icon",
+  profileMenu: ".MuiMenu-root",
+  profileMenuItem: ".MuiMenuItem-root",
+};
